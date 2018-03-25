6fe08eafdccab82b6ff1db6c18d658d50317f6b8
palivoda <ros@palivoda.eu>
2018-03-25 21:06:04 +0300

    added unit tests lib; ducktake js does not start

--- src/EsperScript.hpp
+++ src/EsperScript.hpp
@@ -1,5 +1,31 @@
-#include <duktape.h>
+//https://github.com/dbohdan/embedded-scripting-languages
+
+/*
+Arduino library functions to implement
+
+void pinMode(uint8_t pin, uint8_t mode);
+void digitalWrite(uint8_t pin, uint8_t val);
+int digitalRead(uint8_t pin);
+int analogRead(uint8_t pin);
+void analogReference(uint8_t mode);
+void analogWrite(uint8_t pin, int val);
+void analogWriteFreq(uint32_t freq);
+void analogWriteRange(uint32_t range);
+
+unsigned long millis(void);
+void delay(unsigned long);
+unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout);
+unsigned long pulseInLong(uint8_t pin, uint8_t state, unsigned long timeout);
+
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val);
+uint8_t shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder);
+
+void attachInterrupt(uint8_t pin, void (*)(void), int mode);
+void detachInterrupt(uint8_t pin);
+*/
 
 void esper_script(char* js) {
+
   E_DEBUG("esper_script");
+
 }
